557ef1f5ea525add4ff072347d78842f5a0b6af7
sahilShah55569 <sahilshah2497@gmail.com>
2019-07-03 11:53:45 +0530

    Update Keys.js

--- config/Keys.js
+++ config/Keys.js
@@ -1,5 +1,4 @@
-if(process.env.NODE_ENV === "production") {
-    module.exports = require('./keys_prod')
-}else {
-    module.exports = require('./keys_dev')
-}
+module.exports =  {
+    mongoURI: 'mongodb+srv://shah:Universe@69@cluster0-orelz.mongodb.net/ghargruh?retryWrites=true&w=majority',
+    secretOrKey: 'Stonned'
+} 
